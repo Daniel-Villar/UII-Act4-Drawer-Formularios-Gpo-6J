db60690251c48cba9d8d6ac9525d62c02b9e1720
a.22308051281330 <a.22308051281330@cbtis128.edu.mx>
2025-05-29 18:38:28 +0000

    ora

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -6,28 +6,24 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-            appBar: AppBar(
+      appBar: AppBar(
         elevation: 0.2,
-        title: Text("Daniel Villar", style: TextStyle(color: Colors.white),),
+        title: Text("Daniel Villar"),
         centerTitle: true,
-        backgroundColor: const Color.fromARGB(255, 162, 218, 255),
+        backgroundColor: const Color.fromARGB(255, 70, 107, 131),
         actions: <Widget>[
           InkWell(
             onTap: null,
-            child: IconButton(
-              icon: Icon(
-                Icons.search,
-                color: const Color.fromARGB(255, 0, 0, 0),
-              ), onPressed: () {  },
+            child: new IconButton(
+              icon: Icon(Icons.search, color: Colors.white),
+              onPressed: () {},
             ),
           ),
           InkWell(
             onTap: null,
-            child: IconButton(
-              icon: Icon(
-                Icons.settings,
-                color: const Color.fromARGB(255, 0, 0, 0),
-              ), onPressed: () {  },
+            child: new IconButton(
+              icon: Icon(Icons.settings, color: Colors.white),
+              onPressed: () {},
             ),
           ),
         ],
@@ -36,19 +32,36 @@ class HomePage extends StatelessWidget {
         child: ListView(
           children: [
             UserAccountsDrawerHeader(
-              decoration: BoxDecoration(color: const Color.fromRGBO(123, 202, 255, 1)),
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 76, 93, 112),
+              ),
               accountName: Text("Daniel Villar"),
               accountEmail: Text("a.22308051281330@cbtis128.edu.mx"),
 
               currentAccountPicture: GestureDetector(
                 child: CircleAvatar(
-                radius: 120,
-            child: ClipOval(
-              child: Image.network(
-                'https://raw.githubusercontent.com/Daniel-Villar/Imagenes-para-APP-flutter/refs/heads/main/pp.png',
+                  radius: 130,
+                  backgroundColor: const Color.fromARGB(255, 86, 88, 97),
+                  child: CircleAvatar(
+                    radius: 120,
+                    backgroundImage: NetworkImage(
+                      'https://raw.githubusercontent.com/Daniel-Villar/Imagenes-para-APP-flutter/refs/heads/main/pp.png',
+                    ),
+                  ),
+                ),
               ),
             ),
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                onTap: () {
+                  Navigator.popAndPushNamed(context, "/home");
+                },
+                leading: Icon(
+                  Icons.home,
+                  color: const Color.fromARGB(255, 51, 51, 51),
                 ),
+                title: Text("Pagina inicial"),
               ),
             ),
           ],
